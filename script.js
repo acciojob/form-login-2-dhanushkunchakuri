c7e5464047dcd9ae390634a68ac43ecb1489d10c
Dhanush Kunchakuri <132380514+dhanushkunchakuri@users.noreply.github.com>
2025-05-14 13:24:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+//your JS code here. If required.process.stdout.write("Your message here");
+
+function show() {
+let fname=document.getElementById("name").value;
+let lname=document.getElementById("lname").value;
+let phno=document.getElementById("phno").value;
+let email=document.getElementById("email").value;
+alert(`First Name: ${fname} Last Name: ${lname} Phone Number: ${phno} Email ID: ${email}`);
+document.getElementById("form").reset();
+}
